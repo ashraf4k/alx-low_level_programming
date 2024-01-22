989307af0051c3d45d06dfffc760b8e4b7339a61
ashraf4k <www.ashrafadam000@gmail.com>
2024-01-22 02:41:03 -0800

    a

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef _FUNCION_LIKE_MACRO_H
 #define _FUNCION_LIKE_MACRO_H
 
-#define ABS(x) ((x) < 0 ? (-(x) ) : (x))
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
 #endif /* _FUNCION_LIKE_MACRO_H */
